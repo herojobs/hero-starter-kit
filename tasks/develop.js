44cdfdf3e9c70eef2ad46d684d2ae5204dcc8549
Josh Furie <joshfurie@Joshs-MacBook-Pro.local>
2015-10-17 14:29:21 -0700

    prettify

--- tasks/develop.js
+++ tasks/develop.js
@@ -2,29 +2,33 @@ var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var browserSync = require('browser-sync');
 
-gulp.task('develop', ['clean', 'copy', 'build','browser-sync'], function() {
+gulp.task('develop', ['clean', 'copy', 'build', 'browser-sync'], function() {
   nodemon({
     script: './dist/server.js',
     ext: 'js coffee swig',
     nodeArgs: ['--debug=5878'],
     watch: ['./dist'],
-    delay:2000
+    delay: 2000
   }).on('restart', function() {
 
   });
 
 });
 gulp.task('browser-sync', function() {
-    browserSync.init({
-        reloadDelay: 2000,
-        reloadDebounce:2000,
-        proxy: "http://localhost:3050",
-        port: 3051
-    });
+  browserSync.init({
+    reloadDelay: 2000,
+    reloadDebounce: 2000,
+    proxy: 'http://localhost:3050',
+    port: 3051
+  });
 });
 gulp.task('dist-watch', browserSync.reload);
 
 gulp.task('watch', function() {
-  gulp.watch('./src/**/*',{debounceDelay: 2000}, ['build']);
-  gulp.watch('./dist/**/*',{debounceDelay: 2000}, ['dist-watch']);
+  gulp.watch('./src/**/*', {
+    debounceDelay: 2000
+  }, ['build']);
+  gulp.watch('./dist/**/*', {
+    debounceDelay: 2000
+  }, ['dist-watch']);
 });
